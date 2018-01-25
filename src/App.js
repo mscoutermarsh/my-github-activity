972aeba76c60efc28cb3a8c1e55d8059414104da
Joel Glovier <jglovier@github.com>
2018-01-25 13:21:47 -0500

    test styling

--- src/App.js
+++ src/App.js
@@ -14,12 +14,12 @@ class App extends Component {
     }
 
     return (
-      <div className="App">
-        <header className="App-header">
+      <div className="App Box">
+        <header className="App-header Box-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p className="App-intro Box-body">
           I am {this.props.data.viewer.login}
         </p>
       </div>
