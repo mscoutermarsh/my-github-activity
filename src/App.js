661fa153718d97380004916025a099e903d6d56f
Joel Glovier <jglovier@github.com>
2018-01-25 13:48:34 -0500

    use react-octicons

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.png';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 import './App.css';
+import {CommentIcon} from 'react-octicons'
 
 class App extends Component {
   render(props) {
@@ -15,9 +16,11 @@ class App extends Component {
 
     return (
       <div className="App Box Box--condensed">
+
         <header className="App-header Box-header">
-          <h1 className="App-title Box-title">My GitHub Notifications</h1>
+          <h1 className="App-title Box-title">Recent comments <CommentIcon/></h1>
         </header>
+
         <div className="App-intro Box-body">
           I am {this.props.data.viewer.login}
         </div>
